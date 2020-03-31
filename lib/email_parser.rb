a2dc632af2156f39515cc8d242af48d00a68ac58
Mariam Haider <mfh.haider@gmail.com>
2020-03-31 20:51:55 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,9 +11,7 @@ class EmailParser
 
   def parse
     email_array = @email.split(/[, ]/).uniq
-    email_array.reject! {|element| element.empty?}
+    email_array.reject!{|element| element.empty?}
     email_array
   end
-
-end
 end
